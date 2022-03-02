fb79eaf5ddfb31012b0a39132ac5c00821af43fc
sabrinaelkins <sabrina.elkins12@gmail.com>
2022-03-02 16:54:31 -0600

    updates to portfolio

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import { render } from '@testing-library/react';
+import React from 'react';
+import { HashRouter , Route, Link } from 'react-router-dom';
 import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Project from './components/Project';
+import Resume from './components/Resume';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='base'>
+      <HashRouter basename='sabrina-professional-portfolio/'>
+        <Header></Header>
+        <main className='container my-5'>
+          <Link>
+            <Route exact path={"/"} component={About} />
+            <Route path={"/project"} component={Project} />
+            <Route path={"/contact"} component={Contact} />
+            <Route path={"/resume"} component={Resume} />
+          </Link>
+        </main>
+        <Footer></Footer>
+        </HashRouter>
+        </div>
   );
 }
 
